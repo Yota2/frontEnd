28d5c9fa5a3d15e63bdc4bec46fb0a515fa83b55
Yota <yoni.jmr@gmail.com>
2022-06-23 14:20:25 +0200

    last-opdracht

---
+++ Flex-box-Project/Project-5/Javasccript/mscript.js
@@ -0,0 +1,115 @@
+// console.log(1+2)
+// document.write(" Welcom to JS")
+ document.write("Java script is fun")
+
+ //document.getElementById(pp).innerHTML="Huhuh";
+// let y =5
+// let x =5
+
+// let z = y+x
+// console.log(z)
+
+// const age = 16;
+
+let age = 5;
+// Check if the age is above 16
+
+if (age >= 16) {
+	console.log("You can drink");
+} else {
+	console.log("only Juice");
+}
+
+var possible = (age >= 16) ? "Beer" : "Juice";
+console.log  (possible);
+
+let person = {
+	firstName: "Yohana",
+	age: 100-6,
+	adress: "kessel lo "
+  };
+  
+//   document.querySelector("#data").innerHTML =person.name
+//   document.querySelector("#data").innerHTML =person.firstName + " is " + person.age + " years old.";
+// console.log(person);
+
+// document.querySelector("#tab").innerHTML = <tr>
+// <td> ${person.name}</td>
+// <td> ${person.age}</td>
+// <td> ${person.adress}</td>
+// </tr>
+
+let arr1=[1,2,3,4,5]
+console.log(arr1[3])
+let arr2=["Abel" , "James", "carl"]
+console.log(arr2[2])
+//insertting elements 
+arr2= [...arr2, "Bond"]
+console.log
+arr1.push("Mario")
+console.log(arr1)
+
+//object in array
+// let Students = [
+// 	{name: "jan",
+// age : 22,
+// adress:"Leuven"
+// },
+// {
+// 	name:"Kebede",
+// 	age:33,
+// 	adress:"Kessel lo"
+// },{
+// 	name:"Abebe",
+// 	age:43,
+// 	adress:"Gent"
+// }
+// ]
+let Students = [
+	{name: "jan",
+age : 22,
+adress:"Leuven",
+straat: "Schapenstraat",
+email:"jansmith@gmail.com"
+},
+{
+	name:"Kebede",
+	age:33,
+	adress:"Kessel lo",
+	straat: "Gastoneysekenslaan",
+	email:"jansmith@gmail.com"
+},{
+	name:"Abebe",
+	age:43,
+	adress:"Gent",
+	straat: "Brusselstraat",
+	email:"jansmith@gmail.com"
+}
+]
+console.log(Students)
+console.log(Students[2].name)
+//for loop
+//let isStuden  = "false"
+for(let i = 0;i<Students.length;i++){
+console.log(Students[i].name)
+Students[i].isStudents= true
+}
+console.log(Students)
+//map
+Students.map((Students)=>{
+	console.log(Students)
+})
+
+Students.map((Students)=>{
+    document.querySelector("#tab>tbody").innerHTML += `<tr>
+    <td>${Students.name}</td>
+    <td>${Students.age}</td>
+    <td>${Students.straat}</td>
+    </tr>`
+})
+
+
+// function myfunction(){
+// 	var text = document.getElementById().ariaValueMax;
+// 	alert ("The data is printed");
+// }
---
+++ Flex-box-Project/Tutor-8/app.js
@@ -0,0 +1,105 @@
+let datum = document.getElementById("datum")
+let dag = new Date()
+// document.getElementById = day
+let options={weekday:'short', year: 'numeric' , month: 'long', day: 'numeric'}
+let volDat = dag.toLocaleString('nl-BE' , options)
+datum.innerHTML = volDat
+
+let weekdag = ["sunday", "monday","tusday","wensday","thursday","friday","staturday"]
+
+weekdag = new Date().getDate()
+console.log(weekdag)
+// getdate, getDate(), getMonth 
+
+// let datum1 = document.getElementById("datum1")
+// let Date = new Date().getDate()
+// // document.getElementById = day
+// let maand= ["jan" , "Feb", "maart" , "aprs", "may", "jun"]
+// maand = maand[new Date().getMonth]
+// console.log(maand)
+// let jaar = new Date.getFullyear()
+// console.log(jaar)
+// datum1.innerHTML= weekdag + " "+ Date + " " + maand + ""  + jaar 
+// var welcome;  
+// var person  = prompt("Please enter your name", "Your name here ..... ");
+//     var date = new Date();  
+//     var hour = date.getHours();  
+//     var minute = date.getMinutes();  
+//     var second = date.getSeconds();  
+//     if (minute < 10) {  
+//       minute = "0" + minute;  
+//     }  
+//     if (second < 10) {  
+//       second = "0" + second;  
+//     }  
+//     if (hour < 12) {  
+//       welcome = "Good morning";  
+//     } else if (hour < 17) {  
+//       welcome = "Good afternoon";  
+//     } else {  
+//       welcome = "Good evening";  
+//     }  
+//     document.write("<h2>" + "<font color='red'>" + welcome + ":"   +    person + "</font>" + " welcome ");  
+//     document.write("<br>" + hour + ":" + minute + ":" + second); 
+
+//     // asking user to get
+//     var monthNames = [ "January", "February", "March", "April", "May", "June",
+//     "July", "August", "September", "October", "November", "December" ];
+//       function myFunction() {
+//     var today = new Date();
+//     var date = prompt("Please enter date.", today.getDate()+"-"+monthNames[today.getMonth()]+"-"+today.getFullYear());
+
+//     if (date != null) {
+//         x = "Hello! You have entered date as: " + date;
+//         document.getElementById("demo").innerHTML = x;
+//     }
+// }
+
+
+let result = Math.random()
+result =Math.random()*10
+result = (Math.random(Math.random)*10)
+console.log(result)+1
+
+result = (Math.random(Math.random*9)+1)
+console.log(result)
+
+
+   let speelgoed = ["bal", "pop", "lego","auto", "puzzel", "playstation", "lala","lkia","lulu","dulala"]
+   let kind = speelgoed[Math.round(Math.random()*10)]
+   console.log(kind)
+
+   function test1(parameter1 ,paramter2,paramter3){
+   return console.log(parameter1,paramter2,paramter3)
+}
+test1("hallo",5,"😢","🎶","🎶","😎")
+test1("hallo","freddy",10)
+
+let vrienden = ["els", "marie","linda","karen"]
+
+var studenten=[
+    {
+        id:1,
+        name:"Lala",
+        crusus:"Java",
+        puten:30,
+
+    },
+    {
+        id:2,
+        name:"Tigist",
+        crusus:"Javascript",
+        puten:50,
+
+    },
+    {
+        id:1,
+        name:"Miki",
+        crusus:"python",
+        puten:70,
+
+    },
+]
+
+let higscoor = studenten.filter(puten => puten > 50);
+console.log(higscoor)
